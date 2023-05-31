f86b32e3340ff22564e1f47c335f7c4b7c741e8f
Eduardo Reyes <98363645+eduardoreyes007351208@users.noreply.github.com>
2023-05-31 15:39:27 -0700

    added stuff

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to always remember to reload.
         </p>
         <p>
           Gym Tracking App
